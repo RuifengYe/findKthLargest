0697ef8dcbd31b7aac66081f82a0863846ea5d0f
Ruifeng Ye <yy3194196@gmail.com>
2018-06-07 12:09:38 -0400

    add quicksort method to find the kth largest element

--- src/findKthLargest.java
+++ src/findKthLargest.java
@@ -1,6 +1,6 @@
 public class findKthLargest {
     public static void main(String[] main){
         Integer[] test={5,4,3,6,9,1};
-        //System.out.println(priorityQueueMethod.priority(test2,9));
+        System.out.println(quickSortMethod.quicksort(test,2));
     }
 }
--- src/quickSortMethod.java
+++ src/quickSortMethod.java
@@ -2,24 +2,31 @@ class quickSortMethod {
     public static <AnyType extends Comparable<AnyType>> AnyType quicksort(AnyType[] targetArray,int k){
         if(targetArray==null||targetArray.length<k)
             throw new NullPointerException();
-
+      return new quickSortMethod().quicksortRecursive(targetArray,k,0,targetArray.length-1);
     }
-    private <AnyType extends Comparable<AnyType>> quicksortRecursive(AnyType[] targetArray,int k,int start,int end){
+    private <AnyType extends Comparable<AnyType>> AnyType quicksortRecursive(AnyType[] targetArray,int k,int start,int end){
         int mid=start+end/2;
         swap(targetArray,mid,end);
         int i=start;
-        int j=end-1;
+        int j=end;
         while(i<j)
         {
             while(targetArray[i].compareTo(targetArray[end])<0){
                 i++;
             }
-            while(targetArray[i].compareTo(targetArray[end])>0){
+            while(targetArray[j].compareTo(targetArray[end])>0){
                 j--;
             }
             swap(targetArray,i,j);
         }
         swap(targetArray,i,end);
+        if(i-start+1==k){
+            return targetArray[i];
+        }
+        else if(i-start+1<k){
+            return quicksortRecursive(targetArray,k-i+1,i+1,end);
+        }
+        return quicksortRecursive(targetArray, k, start, i-1);
     }
     private <AnyType extends Comparable<AnyType>> void swap(AnyType[] targetArray,int index1,int index2){
         AnyType tmp=targetArray[index2];
